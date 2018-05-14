607b8c0783a2a3e80bec6d04bff92fc242ccd6d2
CharlesAKA <kwebina-ac@hotmail.co.uk>
2018-05-14 17:25:17 +0100

    Fixed FVX

--- EndersGame/Assets/Scripts/FVX1Spawner.cs
+++ EndersGame/Assets/Scripts/FVX1Spawner.cs
@@ -22,26 +22,36 @@ public class FVX1Spawner : MonoBehaviour
 			CreateLeaders (rndPos);
 		}
 			
+		GameObject leader = GameObject.Instantiate<GameObject> (ssprefab);
+		leader.transform.position = this.transform.TransformPoint (new Vector3(0,-50,0));
+		leader.transform.rotation = this.transform.rotation;
 
-
+		FollowPath fpath = leader.AddComponent<FollowPath> ();
+		fpath.path = GameObject.Find ("Spath").GetComponent<Path> ();
+		fpath.enabled = fpath.enabled;
+		ObstacleAvoidance obavd = leader.AddComponent<ObstacleAvoidance> ();
+		leader.GetComponent<Boid> ().maxSpeed = 30;
+			
 	}
 	void CreateLeaders (Vector3 newpos)
 	{
 		//Create Spaceship 
 		GameObject leader = GameObject.Instantiate<GameObject> (ssprefab);
-		leader.transform.position = this.transform.position;
+		leader.transform.parent = this.transform;
+		leader.transform.position = this.transform.TransformPoint (newpos);
 		leader.transform.rotation = this.transform.rotation;
 
 		Arrive arive = leader.AddComponent<Arrive> ();
 		arive.targetPosition = leader.transform.position + leader.transform.forward * 100;
 		Seek seek = leader.AddComponent<Seek> ();
 		seek.enabled = !seek.enabled;
-
-
+		Path path = leader.AddComponent<Path> ();
+		path.warship = warship;
 		FollowPath fpath = leader.AddComponent<FollowPath> ();
-		fpath.path = GameObject.Find("Spath").GetComponent<Path>();
+		fpath.path = path;
 		fpath.enabled = fpath.enabled;
-		leader.GetComponent<Boid> ().maxSpeed = 50;
+		ObstacleAvoidance obavd = leader.AddComponent<ObstacleAvoidance> ();
+		obavd.enabled = !obavd.enabled;
 
 		for (int i = 1; i <= followers; i++) {
 			Vector3 offset = new Vector3 (gap * i, 0, -gap * i);
@@ -57,12 +67,18 @@ public class FVX1Spawner : MonoBehaviour
 		follower.transform.position = this.transform.TransformPoint (offset);
 		follower.transform.parent = this.transform;
 		follower.transform.rotation = this.transform.rotation;
+
 		OffsetPursue op = follower.AddComponent<OffsetPursue>();
 		op.leader = leader;
-		//Seek seek = follower.AddComponent<Seek> ();
-		//seek.enabled = !seek.enabled;
-
-		//ObstacleAvoidance obavd = follower.AddComponent<ObstacleAvoidance> ();
+		Seek seek = follower.AddComponent<Seek> ();
+		seek.enabled = !seek.enabled;
+		Path path = follower.AddComponent<Path> ();
+		path.warship = warship;
+		FollowPath fpath = follower.AddComponent<FollowPath> ();
+		fpath.path = path;
+		fpath.enabled = fpath.enabled;
+		ObstacleAvoidance obavd = follower.AddComponent<ObstacleAvoidance> ();
+		obavd.enabled = !obavd.enabled;
 
 	}
 
